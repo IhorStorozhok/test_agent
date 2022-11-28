a642ae590d9e00a57eaa51add513bdc90af091d1
IhorStorozhok <83807214+IhorStorozhok@users.noreply.github.com>
2022-11-28 10:34:35 +0000

    Update getinfo.sh

--- getinfo.sh
+++ getinfo.sh
@@ -503,8 +503,7 @@ do
     \"MONITOR_INTERVAL\":\"${HOTHOST_MONITOR_INTERVAL}\"
   }"
 
-  #test2
-  curl --silent --output /dev/null --show-error --fail \
+ curl --silent --show-error 2>&1 | ts | while read line; do echo ${line} ℹ️ HostInfo; done
    -X POST $HOTHOST_SERVER_BASE/api/data/$HOTHOST_AGENT_SECRET \
    -H 'Content-Type: application/json' \
    -d "$JSON_DATA" \
